a83422cb6f085339cf06691e82ca2060d8b0f7fc
Syanmil <syanmil@yahoo.com>
2017-02-21 16:07:42 +0700

    list all houses

--- client/vue.js
+++ client/vue.js
@@ -1,9 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    houses: [],
+    houseInput: {
+      title : '',
+  		price : '',
+  		detail : '',
+  		image : '',
+  		latitude: '',
+      longitude: ''
+    }
   },
   methods: {
-
+    listHouse: function(){
+      axios.get('http://localhost:3000/api/houses')
+      .then(function(response){
+        app.houses = response.data
+      })
+    }
   }
 })
+app.listHouse()
