144a322d7e754cf1c7e5ab549fcea377c540afe9
Syanmil <syanmil@yahoo.com>
2017-02-21 18:04:29 +0700

    sell new house func done

--- client/vue.js
+++ client/vue.js
@@ -21,7 +21,16 @@ var app = new Vue({
     },
     createHouse: function(){
       axios.post('http://localhost:3000/api/houses', {
-
+        title : app.houseCreate.title,
+    		price : app.houseCreate.price,
+    		detail : app.houseCreate.detail,
+    		image : app.houseCreate.image,
+    		latitude: app.houseCreate.latitude,
+        longitude: app.houseCreate.longitude
+      })
+      .then(function(house){
+        console.log('success');
+        app.houses.push(house.data)
       })
     },
     deleteHouse: function(){
